7445b1532a663422d78982318016e38a55a87872
Elgin Davis <elgindavis@college.harvard.edu>
2017-07-29 19:37:01 +0200

    update display name

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 // We import the authentication provider to test the log-out function.
 import { AuthProvider } from '../../providers/auth/auth';
+import firebase from 'firebase';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'page-home',
@@ -9,8 +11,28 @@ import { AuthProvider } from '../../providers/auth/auth';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public authProvider: AuthProvider) {}
+public userProfile:any = null;
+  constructor(public navCtrl: NavController, public authProvider: AuthProvider) {
+     firebase.auth().onAuthStateChanged( user => {
+    if (user) {
 
+
+
+    user.providerData.forEach(function (profile) {
+    console.log("Sign-in provider: "+profile.providerId);
+    console.log("  Provider-specific UID: "+profile.uid);
+    console.log("  Name: "+profile.displayName);
+    console.log("  Email: "+profile.email);
+    console.log("  Photo URL: "+profile.photoURL);
+    })
+    }
+
+     else {
+      console.log("There's no user here");
+    }
+    });
+  }
+  
   /**
    * Calls the authentication provider and logs the user out, on successful logout it sends the user
    * back to the login page.
@@ -18,7 +40,8 @@ export class HomePage {
   logMeOut() {
     this.authProvider.logoutUser().then( () => {
       this.navCtrl.setRoot('LoginPage');
-    });
-  }
+    });}
+
+ 
 
 }
